eeffb5493b90477891b1c2a6a4645cacc82a7ec6
Abdullah <muhammadabdullah7017014@gmail.com>
2024-05-23 07:42:15 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let signinBtn = document.getElementsByClassName(".signin");
+let signupBtn = document.getElementsByClassName(".signup");
+let body = document.getElementsByTagName("body")[0];
+
+function abc() {
+  body.classList.add("slide");
+}
+
+function bbc() {
+  body.classList.remove("slide");
+}
